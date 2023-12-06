8ffd7b95ade730ab1fb4504d1ed31b76657d96cd
jswwWorks <jsww.works@gmail.com>
2023-12-06 18:31:49 -0500

    Added User class

--- models.py
+++ models.py
@@ -1,1 +1,42 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Make instance of SQLAlchemy class to work with
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    # First and last name should be required
+    first_name = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    # TODO: in further study, add a default image
+    image_url = db.Column(
+        db.String(1000),
+        nullable=True
+    )

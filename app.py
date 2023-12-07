5307ad61ef2db996bc0941ddcb7e8a84ae22932e
jswwWorks <jsww.works@gmail.com>
2023-12-06 19:08:13 -0500

    Created HTML template files and added routes to app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +12,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+
+@app.get("/")
+def home_page():
+    """Redirects to users list."""
+
+    return redirect("/users")
+
+
+@app.get("/users")
+def show_users():
+    """Takes nothing, shows tables using SQLAlchemy.
+    Skeleton for application."""
+
+    users = User.query.all()
+    return render_template("list.html", users=users)
+
+
+@app.get("/users/new")
+def show_add_user_form():
+    """Shows new user form."""
+
+    return render_template()
+
+
+@
---
+++ seed.py
@@ -0,0 +1,25 @@
+"""Seed file to make sample data for users db."""
+
+from models import User, db
+from app import app
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# If table isn't empty, empty it
+# An alternative if you don't want to drop
+# and recreate your tables:
+# Pet.query.delete()
+
+# Add pets
+nick = User(first_name='Nick', last_name="Orsi")
+julia = User(first_name='Julia', last_name="Williamson")
+
+
+# Add new objects to session, so they'll persist
+db.session.add(nick)
+db.session.add(julia)
+
+# Commit--otherwise, this never gets saved!
+db.session.commit()

59893c2f5e18d9ab9309223619b1c1d2fe48f7c5
jswwWorks <jsww.works@gmail.com>
2023-12-06 20:46:43 -0500

    Added routes and edit user html file

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import os
 
 from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -13,7 +15,10 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
 
+# FIXME: check on docstrings
 
 @app.get("/")
 def home_page():
@@ -27,8 +32,14 @@ def show_users():
     """Takes nothing, shows tables using SQLAlchemy.
     Skeleton for application."""
 
-    users = User.query.all()
-    return render_template("list.html", users=users)
+    # users = User.query.all()
+    # return render_template("list.html", users=users)
+
+    try:
+        users = User.query.all()
+        return render_template("list.html", users=users)
+    except:
+        return render_template("list.html")
 
 
 @app.get("/users/new")
@@ -47,9 +58,24 @@ def create_new_user():
     image_url=request.form["image_url"]
 
     # TODO: What happens if forms are submitted with info the DB doesn't like?
-    new_user = User(first_name, last_name, image_url)
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    # new_user = User()
+    # db.session.add(new_user)
+    # db.session.commit()
+
+    # new_user.first_name = first_name
+    # new_user.last_name = last_name
+    # new_user.image_url = image_url
+    # db.session.commit()
 
     return redirect("/users")
+
 
 @app.get("/users/<int:user_id>")
 def show_user(user_id):
@@ -60,4 +86,48 @@ def show_user(user_id):
     user = User.query.filter_by(id = f"{user_id}").one()
 
     return render_template("user.html", user=user)
+
+
+@app.get("/users/<int:user_id>/edit")
+def show_edit_user_page(user_id):
+    """Takes user_id, renders edit page for user."""
+
+    user = User.query.filter_by(id = f"{user_id}").one()
+
+    return render_template("edit-user.html", user=user)
+
+
+@app.post("/users/<int:user_id>/edit")
+def process_edit(user_id):
+    """Takes user_id, renders edit page for user."""
+
+    # Grab form inputs
+    first_name=request.form["first_name"]
+    last_name=request.form["last_name"]
+    image_url=request.form["image_url"]
+
+    # Process edits
+    user = User.query.filter_by(id = f"{user_id}").one()
+
+    # Override w/ changes (or leave defaults as need)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+    # TODO: test to make sure edit feature works
+
+    db.session.commit()
+
+    return redirect ("/users")
+
+@app.post("/users/<int:user_id>/delete")
+def delete_user(user_id):
+    """Deletes user from database, redirects to /users homepage.
+    """
+
+    user = User.query.filter_by(id = f"{user_id}").one()
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect ("/users")
+
 
